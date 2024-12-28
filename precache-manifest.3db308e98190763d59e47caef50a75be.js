fc2275ffed1a5cb9d6feb8b3f270a8756d342c5b
kaif-g <kaif2239@gmail.com>
2024-12-28 11:29:53 +0530

    Updates

---
+++ precache-manifest.3db308e98190763d59e47caef50a75be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d4a78b13eb69bcd5537b666dc589e0",
+    "url": "/Real-time-chat-application./index.html"
+  },
+  {
+    "revision": "99f6aa10c7e279b07e9e",
+    "url": "/Real-time-chat-application./static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "4060e67db8a97e87c642",
+    "url": "/Real-time-chat-application./static/js/2.2c124cd5.chunk.js"
+  },
+  {
+    "revision": "99f6aa10c7e279b07e9e",
+    "url": "/Real-time-chat-application./static/js/main.9c90c8fe.chunk.js"
+  },
+  {
+    "revision": "a63cf1479e6a01654d40",
+    "url": "/Real-time-chat-application./static/js/runtime~main.6cea5dd9.js"
+  }
+]);
